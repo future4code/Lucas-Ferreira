cadbeff7f3f24d181e397a3e1d6aa92b1ebcbb2c
unknown <veraslucas669@gmail.com>
2021-06-18 21:25:44 -0300

    Exercicio 15: Objeto com nome Anônimo

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -190,7 +190,10 @@ function criaRetangulo(lado1, lado2) {
 }
 
 // EXERCÍCIO 15
-function anonimizaPessoa(pessoa) {}
+function anonimizaPessoa(pessoa) {
+   pessoa.nome = "ANÔNIMO"
+   return pessoa
+}
 
 // EXERCÍCIO 16A
 function maioresDe18(arrayDePessoas) {}
